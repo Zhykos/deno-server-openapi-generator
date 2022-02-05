4608454c85462634cc3dfa59891669edb077e519
Thomas Cicognani <tcicognani@norsys.fr>
2022-02-05 14:55:19 +0100

    feat(src): first commit with files generated from openapi-generator init script

---
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/DenoOakServerCodegen.java
@@ -0,0 +1,46 @@
+package org.openapitools.codegen.languages;
+
+import org.openapitools.codegen.*;
+import io.swagger.models.properties.ArrayProperty;
+import io.swagger.models.properties.MapProperty;
+import io.swagger.models.properties.Property;
+import io.swagger.models.parameters.Parameter;
+
+import java.io.File;
+import java.util.*;
+
+import org.apache.commons.lang3.StringUtils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DenoOakServerCodegen extends DefaultCodegen implements CodegenConfig {
+    public static final String PROJECT_NAME = "projectName";
+
+    static final Logger LOGGER = LoggerFactory.getLogger(DenoOakServerCodegen.class);
+
+    public CodegenType getTag() {
+        return CodegenType.SERVER;
+    }
+
+    public String getName() {
+        return "deno-oak";
+    }
+
+    public String getHelp() {
+        return "Generates a deno-oak server.";
+    }
+
+    public DenoOakServerCodegen() {
+        super();
+
+        outputFolder = "generated-code" + File.separator + "deno-oak";
+        modelTemplateFiles.put("model.mustache", ".zz");
+        apiTemplateFiles.put("api.mustache", ".zz");
+        embeddedTemplateDir = templateDir = "deno-oak";
+        apiPackage = "Apis";
+        modelPackage = "Models";
+        supportingFiles.add(new SupportingFile("README.mustache", "", "README.md"));
+        // TODO: Fill this out.
+    }
+}
