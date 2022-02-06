716e7b4a8587f3e3649ba5960570199361bd9927
Thomas Cicognani <tcicognani@norsys.fr>
2022-02-06 19:28:55 +0100

    [WIP] feat(app): #2 generate Deno sources from templates
    Generate services and controllers

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/DenoOakServerCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/DenoOakServerCodegen.java
@@ -10,6 +10,9 @@ import java.util.Set;
 import org.openapitools.codegen.CodegenModel;
 import org.openapitools.codegen.CodegenType;
 import org.openapitools.codegen.SupportingFile;
+import org.openapitools.codegen.meta.GeneratorMetadata;
+import org.openapitools.codegen.meta.Stability;
+import org.openapitools.codegen.utils.StringUtils;
 
 public class DenoOakServerCodegen extends AbstractTypeScriptClientCodegen {
     // public static final String PROJECT_NAME = "projectName";
@@ -31,19 +34,34 @@ public class DenoOakServerCodegen extends AbstractTypeScriptClientCodegen {
 
     @Override
     public String getHelp() {
-        return "Generates a deno-oak server.";
+        return "Generates a Deno based oak server.";
     }
 
     public DenoOakServerCodegen() {
         super();
 
+        super.generatorMetadata = GeneratorMetadata.newBuilder(generatorMetadata).stability(Stability.BETA).build();
+
         super.outputFolder = "generated-code" + File.separator + DENO_OAK;
         super.modelTemplateFiles.put("model.mustache", ".ts");
-        super.apiTemplateFiles.put("api.mustache", ".ts");
+        super.apiTemplateFiles.put("controller.mustache", ".ts");
+        super.apiTemplateFiles.put("service.mustache", ".ts");
         super.embeddedTemplateDir = super.templateDir = DENO_OAK;
-        super.apiPackage = "Apis";
-        super.modelPackage = "Models";
+        super.modelPackage = "models";
         super.supportingFiles.add(new SupportingFile("README.mustache", "", "README.md"));
+
+        // controllers folder
+        super.supportingFiles
+                .add(new SupportingFile("controllers" + File.separator + "index.mustache", "controllers", "index.ts"));
+        super.supportingFiles.add(new SupportingFile("controllers" + File.separator + "controller.mustache",
+                "controllers", "Controller.ts"));
+
+        // service folder
+        supportingFiles.add(new SupportingFile("services" + File.separator + "index.mustache", "services", "index.ts"));
+        supportingFiles
+                .add(new SupportingFile("services" + File.separator + "service.mustache", "services", "Service.ts"));
+
+        additionalProperties.put("implFolder", "services");
     }
 
     /* Copied from TypeScriptNodeClientCodegen */
@@ -76,6 +94,41 @@ public class DenoOakServerCodegen extends AbstractTypeScriptClientCodegen {
             }
         }
         return tsImports;
+    }
+
+    @Override
+    public String apiPackage() {
+        return "controllers";
+    }
+
+    @Override
+    public String toApiName(String name) {
+        if (name.length() == 0) {
+            return "Default";
+        }
+        return StringUtils.camelize(name);
+    }
+
+    @Override
+    public String toApiFilename(String name) {
+        return toApiName(name) + "Controller";
+    }
+
+    @Override
+    public String apiFilename(String templateName, String tag) {
+        String result = super.apiFilename(templateName, tag);
+
+        if (templateName.equals("service.mustache")) {
+            String stringToMatch = File.separator + "controllers" + File.separator;
+            String replacement = File.separator + "services" + File.separator;
+            result = result.replace(stringToMatch, replacement).replace("Controller.ts", "Service.ts");
+        }
+        return result;
+    }
+
+    @Override
+    public String apiFileFolder() {
+        return outputFolder + File.separator + apiPackage().replace('.', File.separatorChar);
     }
 
 }
