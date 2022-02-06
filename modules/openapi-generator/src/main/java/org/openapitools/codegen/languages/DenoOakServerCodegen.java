856532825bd105b4fab8d0dc34c8d1a31b110da9
Thomas Cicognani <tcicognani@norsys.fr>
2022-02-06 17:31:33 +0100

    feat(app): #2 generate Deno sources from templates
    Generate model classes

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/DenoOakServerCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/DenoOakServerCodegen.java
@@ -1,32 +1,35 @@
 package org.openapitools.codegen.languages;
 
-import org.openapitools.codegen.*;
-import io.swagger.models.properties.ArrayProperty;
-import io.swagger.models.properties.MapProperty;
-import io.swagger.models.properties.Property;
-import io.swagger.models.parameters.Parameter;
-
 import java.io.File;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
-import org.apache.commons.lang3.StringUtils;
+import org.openapitools.codegen.CodegenModel;
+import org.openapitools.codegen.CodegenType;
+import org.openapitools.codegen.SupportingFile;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class DenoOakServerCodegen extends DefaultCodegen implements CodegenConfig {
+public class DenoOakServerCodegen extends AbstractTypeScriptClientCodegen {
     // public static final String PROJECT_NAME = "projectName";
 
-    private final Logger LOGGER = LoggerFactory.getLogger(DenoOakServerCodegen.class);
+    // private final Logger LOGGER =
+    // LoggerFactory.getLogger(DenoOakServerCodegen.class);
 
+    private static final String DENO_OAK = "deno-oak";
+
+    @Override
     public CodegenType getTag() {
         return CodegenType.SERVER;
     }
 
+    @Override
     public String getName() {
-        return "deno-oak";
+        return DENO_OAK;
     }
 
+    @Override
     public String getHelp() {
         return "Generates a deno-oak server.";
     }
@@ -34,12 +37,45 @@ public class DenoOakServerCodegen extends DefaultCodegen implements CodegenConfi
     public DenoOakServerCodegen() {
         super();
 
-        outputFolder = "generated-code" + File.separator + "deno-oak";
-        modelTemplateFiles.put("model.mustache", ".ts");
-        apiTemplateFiles.put("api.mustache", ".ts");
-        embeddedTemplateDir = templateDir = "deno-oak";
-        apiPackage = "Apis";
-        modelPackage = "Models";
-        supportingFiles.add(new SupportingFile("README.mustache", "", "README.md"));
+        super.outputFolder = "generated-code" + File.separator + DENO_OAK;
+        super.modelTemplateFiles.put("model.mustache", ".ts");
+        super.apiTemplateFiles.put("api.mustache", ".ts");
+        super.embeddedTemplateDir = super.templateDir = DENO_OAK;
+        super.apiPackage = "Apis";
+        super.modelPackage = "Models";
+        super.supportingFiles.add(new SupportingFile("README.mustache", "", "README.md"));
     }
+
+    /* Copied from TypeScriptNodeClientCodegen */
+    @Override
+    public Map<String, Object> postProcessAllModels(final Map<String, Object> objs) {
+        final Map<String, Object> result = super.postProcessAllModels(objs);
+
+        for (final Map.Entry<String, Object> entry : result.entrySet()) {
+            final Map<String, Object> inner = (Map<String, Object>) entry.getValue();
+            final List<Map<String, Object>> models = (List<Map<String, Object>>) inner.get("models");
+            for (final Map<String, Object> mo : models) {
+                final CodegenModel cm = (CodegenModel) mo.get("model");
+
+                // Add additional filename information for imports
+                mo.put("tsImports", toTsImports(cm, cm.imports));
+            }
+        }
+        return result;
+    }
+
+    /* Copied from TypeScriptNodeClientCodegen */
+    private List<Map<String, String>> toTsImports(final CodegenModel cm, final Set<String> imports) {
+        final List<Map<String, String>> tsImports = new ArrayList<>();
+        for (final String im : imports) {
+            if (!im.equals(cm.classname)) {
+                final Map<String, String> tsImport = new HashMap<>();
+                tsImport.put("classname", im);
+                tsImport.put("filename", toModelFilename(im));
+                tsImports.add(tsImport);
+            }
+        }
+        return tsImports;
+    }
+
 }
