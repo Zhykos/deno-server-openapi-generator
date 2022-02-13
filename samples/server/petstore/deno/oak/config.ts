01d1ad4a932cd936c8aa5417520653d874a5432a
Thomas Cicognani <tcicognani@norsys.fr>
2022-02-13 11:23:39 +0100

    [WIP] feat(app): #2 generate Deno sources from templates
    Format samples

--- samples/server/petstore/deno/oak/config.ts
+++ samples/server/petstore/deno/oak/config.ts
@@ -14,6 +14,7 @@ const customConfig = {
 
 export const config = {
   OPENAPI_YAML: join(customConfig.ROOT_DIR, "api", "openapi.yaml"),
-  FULL_PATH: `${customConfig.URL_PATH}:${customConfig.URL_PORT}/${customConfig.BASE_VERSION}`,
+  FULL_PATH:
+    `${customConfig.URL_PATH}:${customConfig.URL_PORT}/${customConfig.BASE_VERSION}`,
   FILE_UPLOAD_PATH: join(customConfig.PROJECT_DIR, "uploaded_files"),
 };
