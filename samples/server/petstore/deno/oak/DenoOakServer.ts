5b6b5dfa95f9f6fd068cd48ec62572c355b8e597
Thomas Cicognani <tcicognani@norsys.fr>
2022-02-21 00:55:46 +0100

    feat(ci): #6 generate specific oak application
    start generating routes

--- samples/server/petstore/deno/oak/DenoOakServer.ts
+++ samples/server/petstore/deno/oak/DenoOakServer.ts
@@ -1,6 +1,12 @@
 import { PetService } from "./services/PetService.ts";
+import { PetPrivateService } from "./services/PetPrivateService.ts";
+import { PetController } from "./controllers/PetController.ts";
 import { StoreService } from "./services/StoreService.ts";
+import { StorePrivateService } from "./services/StorePrivateService.ts";
+import { StoreController } from "./controllers/StoreController.ts";
 import { UserService } from "./services/UserService.ts";
+import { UserPrivateService } from "./services/UserPrivateService.ts";
+import { UserController } from "./controllers/UserController.ts";
 import { DenoServer } from "./DenoServer.ts";
 import { Application } from "./deps.ts";
 
@@ -19,11 +25,22 @@ export class DenoOakServer implements DenoServer {
     myUserService: UserService,
   ): DenoOakServer {
     this.port = port;
-    this.generateRoutes();
+    this.generateRoutes(myPetService, myStoreService, myUserService);
     return this;
   }
 
-  private generateRoutes() {
+  private generateRoutes(
+    myPetService: PetService,
+    myStoreService: StoreService,
+    myUserService: UserService,
+  ) {
+    const privatePetService = new PetPrivateService(myPetService);
+    const privatePetController = new PetController(privatePetService);
+    const privateStoreService = new StorePrivateService(myStoreService);
+    const privateStoreController = new StoreController(privateStoreService);
+    const privateUserService = new UserPrivateService(myUserService);
+    const privateUserController = new UserController(privateUserService);
+
     this.app.use((ctx) => {
       ctx.response.body = "Hello World!";
     });
