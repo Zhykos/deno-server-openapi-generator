8c445b87372fee1cf396fcfcdae61aa53998a3ea
Thomas Cicognani <tcicognani@norsys.fr>
2022-03-08 22:00:16 +0100

    refactor(examples): add an underscore before each var name
    in order to set them as unused

--- samples/server/petstore/deno/oak/DenoOakDummyRoutesExample.ts
+++ samples/server/petstore/deno/oak/DenoOakDummyRoutesExample.ts
@@ -10,37 +10,37 @@ import { User } from "./models/User.ts";
 // Custom services
 
 class MyPetService implements PetService {
-  addPet(pet: Pet): Pet {
+  addPet(_pet: Pet): Pet {
     throw new Error("Method not implemented yet: PetService >> addPet");
   }
-  updatePet(pet: Pet): Pet {
+  updatePet(_pet: Pet): Pet {
     throw new Error("Method not implemented yet: PetService >> updatePet");
   }
   findPetsByStatus(
-    status: Array<"available" | "pending" | "sold">,
+    _status: Array<"available" | "pending" | "sold">,
   ): Array<Pet> {
     throw new Error(
       "Method not implemented yet: PetService >> findPetsByStatus",
     );
   }
-  findPetsByTags(tags: Array<string>): Array<Pet> {
+  findPetsByTags(_tags: Array<string>): Array<Pet> {
     throw new Error("Method not implemented yet: PetService >> findPetsByTags");
   }
-  deletePet(petId: number, apiKey?: string): void {
+  deletePet(_petId: number, _apiKey?: string): void {
     throw new Error("Method not implemented yet: PetService >> deletePet");
   }
-  getPetById(petId: number): Pet {
+  getPetById(_petId: number): Pet {
     throw new Error("Method not implemented yet: PetService >> getPetById");
   }
-  updatePetWithForm(petId: number, name?: string, status?: string): void {
+  updatePetWithForm(_petId: number, _name?: string, _status?: string): void {
     throw new Error(
       "Method not implemented yet: PetService >> updatePetWithForm",
     );
   }
   uploadFile(
-    petId: number,
-    additionalMetadata?: string,
-    file?: any,
+    _petId: number,
+    _additionalMetadata?: string,
+    _file?: any,
   ): ApiResponse {
     throw new Error("Method not implemented yet: PetService >> uploadFile");
   }
@@ -49,43 +49,43 @@ class MyStoreService implements StoreService {
   getInventory(): { [key: string]: number } {
     throw new Error("Method not implemented yet: StoreService >> getInventory");
   }
-  placeOrder(order: Order): Order {
+  placeOrder(_order: Order): Order {
     throw new Error("Method not implemented yet: StoreService >> placeOrder");
   }
-  deleteOrder(orderId: string): void {
+  deleteOrder(_orderId: string): void {
     throw new Error("Method not implemented yet: StoreService >> deleteOrder");
   }
-  getOrderById(orderId: number): Order {
+  getOrderById(_orderId: number): Order {
     throw new Error("Method not implemented yet: StoreService >> getOrderById");
   }
 }
 class MyUserService implements UserService {
-  createUser(user: User): void {
+  createUser(_user: User): void {
     throw new Error("Method not implemented yet: UserService >> createUser");
   }
-  createUsersWithArrayInput(user: Array<User>): void {
+  createUsersWithArrayInput(_user: Array<User>): void {
     throw new Error(
       "Method not implemented yet: UserService >> createUsersWithArrayInput",
     );
   }
-  createUsersWithListInput(user: Array<User>): void {
+  createUsersWithListInput(_user: Array<User>): void {
     throw new Error(
       "Method not implemented yet: UserService >> createUsersWithListInput",
     );
   }
-  loginUser(username: string, password: string): string {
+  loginUser(_username: string, _password: string): string {
     throw new Error("Method not implemented yet: UserService >> loginUser");
   }
   logoutUser(): void {
     throw new Error("Method not implemented yet: UserService >> logoutUser");
   }
-  deleteUser(username: string): void {
+  deleteUser(_username: string): void {
     throw new Error("Method not implemented yet: UserService >> deleteUser");
   }
-  getUserByName(username: string): User {
+  getUserByName(_username: string): User {
     throw new Error("Method not implemented yet: UserService >> getUserByName");
   }
-  updateUser(username: string, user: User): void {
+  updateUser(_username: string, _user: User): void {
     throw new Error("Method not implemented yet: UserService >> updateUser");
   }
 }
