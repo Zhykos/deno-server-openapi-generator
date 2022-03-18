2e617ee44d8534fdb9e2ba401bb4327bbde46f3d
Thomas Cicognani <tcicognani@norsys.fr>
2022-03-18 18:10:46 +0100

    tests(postman): #15 implements "delete user"

--- samples/server/petstore/deno/oak/tests/petstore/MyUserService.ts
+++ samples/server/petstore/deno/oak/tests/petstore/MyUserService.ts
@@ -25,8 +25,16 @@ export class MyUserService implements UserService {
   logoutUser(): void {
     throw new Error("Method not implemented yet: UserService >> logoutUser");
   }
-  deleteUser(_username: string): void {
-    throw new Error("Method not implemented yet: UserService >> deleteUser");
+  deleteUser(username: string): void {
+    // TODO ERROR 400: Invalid user name format (do not know how to check that)
+    const petStoreDB = new PetStoreCompleteExampleDatabase();
+
+    const user: User | undefined = petStoreDB.getUser(username);
+    if (!user) {
+      throw new Deno.errors.NotFound(
+        `Cannot delete user with ID: '${username}'`,
+      );
+    }
   }
   getUserByName(username: string): User {
     // TODO ERROR 400: Invalid user name format (do not know how to check that)
@@ -44,7 +52,9 @@ export class MyUserService implements UserService {
 
     const existingUser: User | undefined = petStoreDB.getUser(username);
     if (!existingUser) {
-      throw new Deno.errors.NotFound(`Cannot update user with ID: '${username}'`);
+      throw new Deno.errors.NotFound(
+        `Cannot update user with ID: '${username}'`,
+      );
     }
     existingUser.copyFrom(user);
   }
