a4fe29894b7e0c72fc543e419a1109dc7b645aab
Thomas Cicognani <github@tomtom.email>
2023-06-24 22:59:02 +0200

    feat: update copyright for previously updated files

--- modules/openapi-generator/src/test/java/org/openapitools/codegen/deno/DenoServerCodegenTest.java
+++ modules/openapi-generator/src/test/java/org/openapitools/codegen/deno/DenoServerCodegenTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 Thomas "Zhykos" Cicognani
+ * Copyright 2023 Thomas "Zhykos" Cicognani
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
